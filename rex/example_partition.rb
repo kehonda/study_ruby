39f3f5a309b66f7e6811f0930209dfd747d4fbee
kehonda <731.kentaro@gmail.com>
2020-10-10 15:44:50 +0900

    よくわからない・・

--- rex/example_partition.rb
+++ rex/example_partition.rb
@@ -1,8 +1,11 @@
-a, = (1..5).partition(&:odd?)
-p a
+# a, = (1..5).partition(&:odd?)
+# p a
 
-a = (1..5).partition(&:odd?)
-p a
+# a = (1..5).partition(&:odd?)
+# p a
 
-,a = (1..5).partition(&:odd?)
+# ,a = (1..5).partition(&:odd?)
+# p a
+
+a[1] = (1..5).partition(&:odd?)
 p a
