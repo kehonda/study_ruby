264dd84856f0c19973f71f212029415b3c39b0b4
kehonda <731.kentaro@gmail.com>
2020-10-14 23:12:03 +0900

    String#indexの結果はindex位置に限らず左から数えて何番めか

---
+++ example_string_index.rb
@@ -0,0 +1,18 @@
+ 
+p "Ruby!Ruby!Ruby!".index("!", 2)
+
+p "Ruby!Ruby!Ruby!".index("!", 0)
+
+p "Ruby!Ruby!Ruby!".index("!", 3)
+
+p "Ruby!Ruby!Ruby!".index("!", 4)
+
+p "Ruby!Ruby!Ruby!".index("!", 5)
+
+p "Ruby!Ruby!Ruby!".index("!", 6)
+
+p "Ruby0Ruby!Ruby!".index("0", 3)
+
+p "Ruby0Ruby!Ruby!".index("0", 4)
+
+p "Ruby0Ruby!Ruby!".index("0", 5)
